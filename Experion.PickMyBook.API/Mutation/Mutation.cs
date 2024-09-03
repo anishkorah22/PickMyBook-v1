3eb8a9461ea12f9473656432aa0e4c4573b69468
Gopika Vinod <gopikavinod20@gmail.com>
2024-09-03 14:32:38 +0530

    Added scope

--- Experion.PickMyBook.API/Mutation/Mutation.cs
+++ Experion.PickMyBook.API/Mutation/Mutation.cs
@@ -9,7 +9,7 @@ using HotChocolate.Subscriptions;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
-public class Mutation
+/*public class Mutation
 {
     private readonly IBookService _bookService;
     private readonly IUserService _userService;
@@ -112,4 +112,153 @@ public class Mutation
         return updatedUser;
     }
 
+}*/
+
+public class Mutation
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public Mutation(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public async Task<Book> AddBook(AddBooksDTO dto)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetService<LibraryContext>();
+        var bookService = scope.ServiceProvider.GetService<IBookService>();
+        return await bookService.AddBookAsync(dto);
+    }
+
+    public async Task<Book> UpdateBook(Book book)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetService<LibraryContext>();
+        var bookService = scope.ServiceProvider.GetService<IBookService>();
+        return await bookService.UpdateBookAsync(book);
+    }
+
+    [Authorize(Roles = new[] { "Admin" })]
+    public async Task<User> AddUser(User user)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetService<LibraryContext>();
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        return user;
+    }
+
+    public async Task<Borrowings> BorrowBook(int userId, int bookId)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var borrowingService = scope.ServiceProvider.GetService<IBorrowingService>();
+        return await borrowingService.BorrowBookAsync(userId, bookId);
+    }
+
+    public async Task<Borrowings> UpdateBorrowing(Borrowings borrowing)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var borrowingService = scope.ServiceProvider.GetService<IBorrowingService>();
+        return await borrowingService.UpdateBorrowingAsync(borrowing);
+    }
+
+    public async Task<Borrowings> ReturnBook(int userId, int bookId)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var borrowingService = scope.ServiceProvider.GetService<IBorrowingService>();
+        return await borrowingService.ReturnBookAsync(userId, bookId);
+    }
+
+    public async Task<Request> CreateBorrowRequestAsync(int bookId, int userId)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var requestService = scope.ServiceProvider.GetService<IRequestService>();
+        return await requestService.CreateBorrowRequestAsync(bookId, userId);
+    }
+
+    public async Task<Request> CreateReturnRequestAsync(int bookId, int userId)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var requestService = scope.ServiceProvider.GetService<IRequestService>();
+        return await requestService.CreateReturnRequestAsync(bookId, userId);
+    }
+
+    public async Task<Request> ApproveRequestAsync(int requestId)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var requestService = scope.ServiceProvider.GetService<IRequestService>();
+        return await requestService.ApproveRequestAsync(requestId);
+    }
+
+    public async Task<Request> DeclineRequestAsync(int requestId, string message)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var requestService = scope.ServiceProvider.GetService<IRequestService>();
+        return await requestService.DeclineRequestAsync(requestId, message);
+    }
+
+    public async Task<User> CreateUser(string userName, IEnumerable<string> roles)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetService<LibraryContext>();
+        var userService = scope.ServiceProvider.GetService<IUserService>();
+        var newUser = new User
+        {
+            UserName = userName,
+            Roles = roles,
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
+        };
+
+        context.Users.Add(newUser);
+        await context.SaveChangesAsync();
+        return newUser;
+    }
+
+    public async Task<User> UpdateUser(User user)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var context = scope.ServiceProvider.GetService<LibraryContext>();
+        var userService = scope.ServiceProvider.GetService<IUserService>();
+        var existingUser = await context.Users.FindAsync(user.UserId);
+
+        if (existingUser == null)
+        {
+            throw new KeyNotFoundException("User not found.");
+        }
+
+        existingUser.UserName = user.UserName;
+        existingUser.Roles = user.Roles;
+        existingUser.IsDeleted = user.IsDeleted;
+        existingUser.UpdatedAt = DateTime.UtcNow;
+
+        context.Users.Update(existingUser);
+        await context.SaveChangesAsync();
+
+        return existingUser;
+    }
+
+    public async Task<Book> UpdateBookStatusAsync(int bookId, bool isDeleted)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var bookService = scope.ServiceProvider.GetService<IBookService>();
+        var eventSender = scope.ServiceProvider.GetService<ITopicEventSender>();
+        var updatedBook = await bookService.UpdateBookStatusAsync(bookId, isDeleted);
+
+        await eventSender.SendAsync("OnBookStatusChanged", updatedBook);
+
+        return updatedBook;
+    }
+    public async Task<User> UpdateUserStatusAsync(int userId, bool isDeleted)
+    {
+        using var scope = _serviceScopeFactory.CreateScope();
+        var userService = scope.ServiceProvider.GetService<IUserService>();
+        var eventSender = scope.ServiceProvider.GetService<ITopicEventSender>();
+        var updatedUser = await userService.UpdateUserStatusAsync(userId, isDeleted);
+
+        await eventSender.SendAsync("OnUserStatusChanged", updatedUser);
+
+        return updatedUser;
+    }
 }
